c2051cb5375b91356e184fc37e4d983e0168b1f4
vitaly-gryaznov <Vitaly.Gryaznov@esailors.de>
2022-02-13 13:56:07 +0100

    fix google analytics

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
+import React, { useEffect } from "react";
 import Main from "./Main";
 import CookiesConsent from "./CookieConsent";
 import Footer from "./Footer";
 import Header from "./header/Header";
+import ReactGA from "react-ga";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-192948383-1");
+    ReactGA.pageview("/");
+  }, []);
+
   return (
     <div>
       <Header />
